cd567dab934de1725da64bb095674db81a0bbc33
“[rahma <[rahmahfaiedh5@gmail.com]>
2022-03-03 01:47:11 +0000

    03032022 02:45

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
     const pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'ROOT',
         database: 'backsql'
     });
     
--- src/Controllers/EtudiantController.js
+++ src/Controllers/EtudiantController.js
@@ -17,18 +17,10 @@ exports.loadStudentDetails= async (req,res,next)=>{
     for(var j=0;j<results.length;j++)
     {
       tabTest =new Array();
-//console.log("j",j);
         const classeEtud= await classe.fetchById(results[j].id_classe);
         
         const resultclasseEtud = classeEtud[0][0];
-       // console.log("resultclasseEtud",resultclasseEtud);
-      
-    
-   
-      // recupere test et res 
-
-        const datarestest = await resTest.fetchResultatsByUser(results[j].id_user);
-      //  console.log("datarestest",datarestest[0]);
+          const datarestest = await resTest.fetchResultatsByUser(results[j].id_user);
       
         for(var i=0;i<datarestest[0].length;i++){
          console.log("iiiiiiiiiiii",i);
@@ -37,20 +29,9 @@ exports.loadStudentDetails= async (req,res,next)=>{
             const datatest = await test.fetchByIdTest(datarestest[0][i].idTest);
           console.log("resultTestEtudiant",resultTestEtudiant);
         
-          // for(var a=0;a<datatest[0].length;a++){
-          //   tabDataTest.push(datatest[a]);
-         
-          // }
+       
           
       
-        // tabDataTest.push(datatest[0][0]);
-        
-       // console.log("datatest[j]",datatest[j]);
-        //  console.log("tabDataTest",tabDataTest[i]);
- 
-     
-       //  console.log("tests",tests);
-        // console.log("tabTest",tabTest);
         tests={
           resultatTestEtud:resultTestEtudiant,
           testEtud:datatest[0]
@@ -79,24 +60,4 @@ exports.loadStudentDetails= async (req,res,next)=>{
   }
 
 
-    // recupere etudiant
-
-        // let id_user=results[j].id_user;
-        // let lastName=results[j].lastName;
-        // let firstName=results[j].firstName;
-        // let email=results[j].email;
-        // let photo=results[j].photo;
-        // let phone=results[j].phone;
-        // let birthDate=results[j].birthDate;
-        // let pwd=results[j].pwd;
-        // let type=results[j].type;
-        // let invitation=results[j].invitation;
-        // let creationDateEtudiant=results[j].creationDate;
-       
-          // recupere classe
-
-        //   let nameClasse=resultclasseEtud.name;
-        //   let shortNamelasse=resultclasseEtud.shortName;
-        //   let year=resultclasseEtud.year;
-        //   let department=resultclasseEtud.department;
-        //   let creationDateClasse=resultclasseEtud.creationDate;
+   
