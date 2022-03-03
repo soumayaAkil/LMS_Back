e31e848b32f31bc1cbcc69a10d5f6790ed9aa354
“[rahma <[rahmahfaiedh5@gmail.com]>
2022-03-03 15:28:28 +0000

    03032022 16:30

--- src/Models/resTestModel.js
+++ src/Models/resTestModel.js
@@ -1,11 +1,11 @@
 const db = require('../../Config/database');
 
 module.exports = class resTest {
-    constructor(id_res,id_user,idTest,Marks){
+    constructor(id_res,id_user,idTest,totalMarks){
         id_res=this.id_res;
         id_user=this.id_user;
         idTest=this.idTest;
-        Marks=this.Marks;
+        totalMarks=this.totalMarks;
     }
 
     static fetchResultatsByUser(id_user) {
