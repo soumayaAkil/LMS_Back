8cec356bbea284623dfbbd4d411774305b2277b2
soumayaAkil <85191150+soumayaAkil@users.noreply.github.com>
2022-03-06 00:01:58 +0100

    commits00

--- src/Routes/EtudiantRoute.js
+++ src/Routes/EtudiantRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const EtudiantController=require("../Controllers/EtudiantController");
 
 router.get('/LoadStudentDetail', EtudiantController.loadStudentDetails);
+router.get('/EtudiantNONC', EtudiantController.getEtudNonc);
 
 module.exports=router;  
